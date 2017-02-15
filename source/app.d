c0b41ca004edec9a0ee7ba27c37cdb9f9ac2f2bb
Kevin Gori <kgori@ebi.ac.uk>
2017-02-15 16:53:22 +0000

    Bug: pileup doesn't cross ref_ids

--- source/app.d
+++ source/app.d
@@ -1,7 +1,11 @@
+import bio.bam.read: compareCoordinates;
 import bio.bam.reader;
+import bio.bam.writer;
 import bio.bam.pileup;
-import std.algorithm: filter, reduce;
+import utils.progressbar;
+import std.algorithm: filter, reduce, sort;
 import std.conv;
+import std.functional: toDelegate;
 import std.parallelism;
 import std.stdio;
 
@@ -20,19 +24,19 @@ double avg_base_quality(R)(const ref R r) {
 	return qual / r.base_qualities.length;
 }
 
-// void progress(lazy float p) {
-// 	static uint n;
-// 	if(++n % 100 == 0) {
-// 		writeln(p)
-// 	}
-// }
+void progress(lazy float p) {
+	static uint n;
+	if(++n % 100 == 0) {
+		writeln(p);
+	}
+}
 
 void main(string[] argv)
 {
 	// Argument handling (do properly later)
-	if (argv.length != 3)
+	if (argv.length != 4)
 	{
-		writefln("Usage: %s <threads> <bamfile>", argv[0]);
+		writefln("Usage: %s <threads> <bamfile:in> <bamfile:out>", argv[0]);
 		import std.c.process: exit;
 		exit(-1);
 	} 
@@ -43,48 +47,97 @@ void main(string[] argv)
 	auto pool = new TaskPool(nthreads);
 	scope(exit) pool.finish();
 
-	// Open the bam file with a parallel reader
-	auto bam = new BamReader(filename, pool);
+	// Open the input and output bam files with a parallel pool
+	auto reader = new BamReader(filename, pool);
+	auto writer = new BamWriter(argv[3], -1, pool);
+	scope(exit) writer.finish();
+	writer.writeSamHeader(reader.header);
+	writer.writeReferenceSequenceInfo(reader.reference_sequences);
 
 	// Iterate over the bam file
-	BamRead[] both_unmapped;
-	BamRead[] mate_mapped;
-	BamRead[] i_am_mapped_my_mate_is_not;
 	BamRead[] [string] data;
 
-	foreach (read; filter!(passes_initial_checks)(bam.reads)) {
-		data[read.name] ~= read;
-		if (read.is_unmapped && avg_base_quality(read) > 15) {
-			if (read.mate_is_unmapped) {
-			    both_unmapped ~= read;
-		    }
-			else {
-				mate_mapped ~= read;
-			}
-		}
-		else if (read.mate_is_unmapped && read.mapping_quality > 30) {
-			i_am_mapped_my_mate_is_not ~= read;
-		}
+	auto bar = new shared(ProgressBar)(100000);
+	foreach (read; (reader.readsWithProgress((lazy float p) { bar.update(p); }))) {
+		if (passes_initial_checks(read)) data[read.name] ~= read;
 	}
+	bar.finish();
 	
-	writefln("Found %d half-unmapped pairs, and %d fully unmapped pairs",
-	    mate_mapped.length, both_unmapped.length);
-	writefln("Found %d mapped partners of half-unmapped pairs",
-	    i_am_mapped_my_mate_is_not.length);
 	writefln("Found %d total", data.length);
 
 	foreach (key; data.keys) {
-		writefln("%s\t%d", key, data[key].length);
-	}
-	// TODO: Collect the mapped mates of our mate_mapped reads and build pileup
-	auto pileup = makePileup(i_am_mapped_my_mate_is_not);
-	
-	foreach (column; pileup) {
-		if (column.reference_base == 'N') continue;
-		writeln("Column position: ", column.position);
-		writeln("    Ref.base: ", column.reference_base); // extracted from MD tags
-		writeln("    Coverage: ", column.coverage);
+		sort!("a.is_first_of_pair")(data[key]);
+		if (data[key].length > 1) assert(data[key][0].is_first_of_pair && data[key][1].is_second_of_pair);
 	}
 
+	// Get reads for pileup
+	BamRead[] mapped;
+
+	foreach(readList; data.values) {
+		if (readList.length < 2) continue;
+		if (readList[1].is_unmapped && readList[0].mapping_quality > 50 && avg_base_quality(readList[1]) > 15) {
+			mapped ~= readList[0];
+		}
+		else if (readList[0].is_unmapped && readList[1].mapping_quality > 50 && avg_base_quality(readList[0]) > 15) {
+			mapped ~= readList[1];
+		}
+	}
+	sort!compareCoordinates(mapped);
+	writefln("Found %d reads for pileup.", mapped.length);
+	auto debug_ = new BamWriter("debug.bam", -1, pool);
+	scope(exit) debug_.finish();
+	debug_.writeSamHeader(reader.header);
+	debug_.writeReferenceSequenceInfo(reader.reference_sequences);
+	foreach(read; mapped) debug_.writeRecord(read);
+
+	// Pileup to select reads that are in regions that achieve a certain coverage
+	auto pileup = makePileup(mapped, true);
+	foreach (column; pileup) {
+		if (column.coverage > 0) {
+			foreach (read; column.reads) {
+				
+                writefln("%30s\t%s\t%.2d\t%s\t%2s/%2s\t%2s/%2s\t%10s\t%s %s", 
+                        read.name, 
+                        read.current_base,
+                        read.current_base_quality,
+                        read.cigar_operation,
+                        read.cigar_operation_offset + 1, read.cigar_operation.length,
+                        read.query_offset + 1, read.sequence.length,
+                        read.cigarString(),
+                        read.cigar_before, read.cigar_after);
+        
+				if (read.name in data) {
+					foreach (cached_read; data[read.name]) {
+						writer.writeRecord(cached_read);
+					}
+					data.remove(read.name);
+				}
+			}
+		}
+	}
+	pileup.popFront();
+	foreach (column; pileup) {
+		if (column.coverage > 0) {
+			foreach (read; column.reads) {
+				
+                writefln("%30s\t%s\t%.2d\t%s\t%2s/%2s\t%2s/%2s\t%10s\t%s %s", 
+                        read.name, 
+                        read.current_base,
+                        read.current_base_quality,
+                        read.cigar_operation,
+                        read.cigar_operation_offset + 1, read.cigar_operation.length,
+                        read.query_offset + 1, read.sequence.length,
+                        read.cigarString(),
+                        read.cigar_before, read.cigar_after);
+        
+				if (read.name in data) {
+					foreach (cached_read; data[read.name]) {
+						writer.writeRecord(cached_read);
+					}
+					data.remove(read.name);
+				}
+			}
+		}
+	}
 	writeln("Done.");
 }
