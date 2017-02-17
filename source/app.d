3480c5bafcb48937d7d5f380be798c05a4d9d871
Kevin Gori <kgori@ebi.ac.uk>
2017-02-17 12:21:08 +0000

    Reduced memory requirements

--- source/app.d
+++ source/app.d
@@ -1,15 +1,18 @@
 import bio.bam.read: compareCoordinates;
-import bio.bam.reader;
-import bio.bam.writer;
-import bio.bam.pileup;
-import utils.progressbar;
+import bio.bam.region: BamRegion;
+import bio.bam.reader: BamReader;
+import bio.bam.writer: BamWriter;
+import bio.bam.pileup: pileupChunks;
+import utils.progressbar: ProgressBar;
 import std.algorithm: filter, reduce, sort;
-import std.conv;
+import std.conv: to;
+import std.file: remove;
 import std.functional: toDelegate;
-import std.parallelism;
-import std.stdio;
+import std.parallelism: TaskPool;
+import std.range: iota, tee;
+import std.stdio: writeln, writefln;
 
-bool passes_initial_checks(R)(const ref R r) {
+bool passes_initial_checks(R)(const R r) {
 	return (!r.is_duplicate &&
 		    r.is_paired &&
 			!r.proper_pair &&
@@ -19,9 +22,34 @@ bool passes_initial_checks(R)(const ref R r) {
 			(r.is_unmapped || r.mate_is_unmapped));
 }
 
+// Use with std.functional.partial to fix the qual values
+bool passes_quality_checks(R)(const R r, int base_qual, int map_qual) {
+	return r.is_unmapped ? avg_base_quality(r) >= base_qual : r.mapping_quality >= map_qual;
+}
+
 double avg_base_quality(R)(const ref R r) {
 	auto qual = reduce!"a + b"(0.0, r.base_qualities);
 	return qual / r.base_qualities.length;
+}
+
+void write_unmapped(Read, FileHandle)(ref Read r, ref FileHandle file) {
+	if (r.is_unmapped) file.writeRecord(r);
+}
+
+bool overlaps(Read, Region)(Read read, Region region, bool use_mate_info=false) {
+	auto read_start = use_mate_info ? read.mate_position : read.position;
+	auto read_ref_id = use_mate_info ? read.mate_ref_id : read.ref_id;
+	return (read_ref_id == region.ref_id && 
+	    	read_start <= region.end && 
+			read_start + (use_mate_info ? read.sequence_length : read.basesCovered()) > region.start);
+}
+
+auto new_bamwriter_from_template(string filename, ref BamReader reader, ref TaskPool taskpool) {
+	// Remember to call scope(exit) writer.finish() on the returned writer
+	auto writer = new BamWriter(filename, -1, taskpool);
+	writer.writeSamHeader(reader.header);
+	writer.writeReferenceSequenceInfo(reader.reference_sequences);
+	return writer;
 }
 
 void main(string[] argv)
@@ -40,8 +68,8 @@ void main(string[] argv)
 	string au_out = argv[5];
 
 	// Collect thresholds (TODO: make getopt options)
-	int MAPQUAL = 50;
-	int BASEQUAL = 15;
+	int MAPQUAL = 30;
+	int BASEQUAL = 10;
 	int MINCOV = 1;
 
 	// Set up parallelism for bam reader
@@ -50,95 +78,156 @@ void main(string[] argv)
 
 	// Open the input bam files with a parallel pool
 	auto reader = new BamReader(fl_in, pool);
-
-	// Iterate over the bam file
-	BamRead[] [string] data;
-
-	auto bar = new shared(ProgressBar)(100000);
-	foreach (read; (reader.readsWithProgress((lazy float p) { bar.update(p); }))) {
-		if (passes_initial_checks(read)) data[read.name] ~= read;
-	}
-	bar.finish();
 	
-	writefln("Found %d total", data.length);
+	// Declare regionlist in this scope
+	BamRegion[] regions;
+	bool[string] cache;	
 
-	foreach (key; data.keys) {
-		sort!("a.is_first_of_pair")(data[key]);
-		if (data[key].length > 1) assert(data[key][0].is_first_of_pair && data[key][1].is_second_of_pair);
-	}
-
-	// Get reads for pileup
-	BamRead[] mapped;
-	BamRead[] both_unmapped;
-
-	foreach(readList; data.values) {
-		if (readList.length < 2) continue;
-		if (readList[1].is_unmapped && readList[0].mapping_quality > MAPQUAL && avg_base_quality(readList[1]) > BASEQUAL) {
-			mapped ~= readList[0];
-		}
-		else if (readList[0].is_unmapped && readList[1].mapping_quality > MAPQUAL && avg_base_quality(readList[0]) > BASEQUAL) {
-			mapped ~= readList[1];
-		}
-		else if (readList[0].is_unmapped && readList[1].is_unmapped && avg_base_quality(readList[0]) > BASEQUAL && avg_base_quality(readList[1]) > BASEQUAL) {
-			both_unmapped ~= readList[0];
-			both_unmapped ~= readList[1];
-		}
-	}
-	sort!compareCoordinates(mapped);
-	writefln("Found %d reads for pileup.", mapped.length);
-	writefln("Found %d doubly-unmapped read pairs", both_unmapped.length);
-
-	// Write the doubly-unmapped reads
-	if (both_unmapped.length > 0) {
-		auto all_unmapped_writer = new BamWriter(au_out, -1, pool);
-		scope(exit) all_unmapped_writer.finish();
-		all_unmapped_writer.writeSamHeader(reader.header);
-		all_unmapped_writer.writeReferenceSequenceInfo(reader.reference_sequences);
-		sort!compareCoordinates(both_unmapped);
-		foreach(read; both_unmapped) {
-			all_unmapped_writer.writeRecord(read);
-		}
-	}
+	// 1: Select read pairs where at least one read is unmapped
+	// 2: Apply quality filters, 
+	//    i.e. mapped reads >=MAPQUAL mapping quality, unmapped >=BASEQUAL avg base quality
+	// 3: Divert half-mapped, half-unmapped and fully unmapped reads into separate files 
+	// -caveat: using assoc array 'cache' to keep track of read pairing
+	{
+		writefln("Searching %s for unmapped reads", fl_in);
+		auto bar = new shared(ProgressBar)(100000);
+		scope(exit) bar.finish();
+		int nreads = 0;
+		auto unmapfile = new_bamwriter_from_template("tmp_unmapped.bam", reader, pool);
+		scope(exit) unmapfile.finish();
+		auto doubleunmapfile_1 = new_bamwriter_from_template(au_out, reader, pool);
+		scope(exit) doubleunmapfile.finish();
+		auto mapfile = new_bamwriter_from_template("tmp_mapped.bam", reader, pool);
+		scope(exit) mapfile.finish();
+		
+		auto readGetter = reader.readsWithProgress((lazy float p) { bar.update(p); })
+								.filter!(passes_initial_checks)
+								.filter!(r => passes_quality_checks(r, BASEQUAL, MAPQUAL));
 	
-	// Pileup to select reads that are in regions that achieve a certain coverage
-	if (mapped.length > 0) {
-		
-		// Now open the output bam, reusing the Task Pool
-		auto half_mapped_writer = new BamWriter(hm_out, -1, pool);
-		scope(exit) half_mapped_writer.finish();
-		half_mapped_writer.writeSamHeader(reader.header);
-		half_mapped_writer.writeReferenceSequenceInfo(reader.reference_sequences);
+		foreach (read; readGetter) {
+			nreads++;
+			if (read.is_unmapped) {
+				if (read.mate_is_unmapped) {
+					doubleunmapfile.writeRecord(read);
+				}
+				else {
+					unmapfile.writeRecord(read);
+					cache[read.name] = true;
+				}
+			}
+			else {
+			    mapfile.writeRecord(read);
+			}
+		}
+	}
 
-		auto half_unmapped_writer = new BamWriter(hu_out, -1, pool);
-		scope(exit) half_unmapped_writer.finish();
-		half_unmapped_writer.writeSamHeader(reader.header);
-		half_unmapped_writer.writeReferenceSequenceInfo(reader.reference_sequences);
+	// 1: Reopen half-mapped reads file
+	// 2: Filter for reads that have a quality passing unmapped pair (in cache)
+    //    and divert to filtered file
+	// 3: Pileup half-mapped reads and identify regions where depth > MINCOV
+	{
+		writeln("Checking coverage of filtered reads");
+		auto bar = new shared(ProgressBar)(1000);
+		scope(exit) bar.finish();
 		
-		foreach(chunk; pileupChunks(mapped, true)) {
-			foreach (column; chunk) {
+		auto mReader = new BamReader("tmp_mapped.bam", pool);
+		scope(exit) {
+			remove("tmp_mapped.bam");
+		    remove("tmp_mapped.bam.bai");
+		}
+		
+		auto filteredmapfile = new_bamwriter_from_template("tmp_mapped_filtered.bam", mReader, pool);
+		scope(exit) filteredmapfile.finish();
+		
+		auto mReadGetter = mReader.readsWithProgress((lazy float p) { bar.update(p); })
+							.filter!(r => r.name in cache)
+							.tee!(r => filteredmapfile.writeRecord(r));
+		
+		// Now do pileup and identify regions
+		foreach(chunk; pileupChunks(mReadGetter, true)) {
+			
+			int refid = 0; // Region delimiters
+			ulong left = 0;
+			ulong right = 0;
+			bool initialised = false;
+			
+			foreach(column; chunk) {
 				if (column.coverage > MINCOV) {
-					foreach (read; column.reads) {
-						if (read.name in data) {
-							BamRead[] readlist = data[read.name];
-							if (!readlist[0].is_unmapped && readlist[1].is_unmapped) {
-								half_mapped_writer.writeRecord(readlist[0]);
-								half_unmapped_writer.writeRecord(readlist[1]);
-							} 
-							else if (readlist[0].is_unmapped && !readlist[1].is_unmapped) {
-								half_unmapped_writer.writeRecord(readlist[0]);
-								half_mapped_writer.writeRecord(readlist[1]);
-							}
-							data.remove(read.name);
-						}
+					if (!initialised) {
+						initialised = true;
+						refid = column.ref_id;
+						left = column.position;
+						right = column.position;
 					}
+					right = column.position;
+				}
+				else if (initialised) {
+					auto region = BamRegion(refid, to!uint(left), to!uint(right));
+					regions ~= region;
+					initialised = false;
+					refid = left = right = 0;
 				}
 			}
 		}
-		writefln("Completed. Half mapped reads written to %s.", hm_out);
-		writefln("           Half unmapped reads written to %s.", hu_out);
-		writefln("           Fully unmapped reads written to %s.", au_out);
+		cache = cache.init; // kill the cache
 	}
-	else {
-		writeln("Completed, but no unmapped reads found.");
+
+	// 1: Reopen cached unmapped reads
+	// 2: Filter for reads paired to coverage-passing mapped reads
+	{
+		writeln("Writing half-unmapped reads tied to high coverage areas");
+		reader = new BamReader("tmp_unmapped.bam", pool);
+		int i=0;
+		auto region = regions[i];
+		
+		auto writer = new_bamwriter_from_template(hu_out, reader, pool);
+		scope(exit) {
+			remove("tmp_unmapped.bam");
+			remove("tmp_unmapped.bam.bai");
+			writer.finish();
+		}
+
+		foreach (read; reader.reads) {
+			if (overlaps(read, region, true)) {
+				writer.writeRecord(read);
+			}
+			else if (region.fullyLeftOf(read.ref_id, read.mate_position)) {
+				i++;
+				if (i >= regions.length) break;
+				region = regions[i];
+			}
+		}
 	}
+
+	// 1: Reopen cached mapped reads
+	// 2: Filter for coverage-passing reads
+	{
+		writeln("Writing half-mapped reads tied to high coverage areas");
+		reader = new BamReader("tmp_mapped_filtered.bam", pool);
+		int i=0;
+		auto region = regions[i];
+		
+		auto writer = new_bamwriter_from_template(hm_out, reader, pool);
+		scope(exit) {
+			remove("tmp_mapped_filtered.bam");
+			remove("tmp_mapped_filtered.bam.bai");
+			writer.finish();
+		}
+		
+		foreach (read; reader.reads) {
+			if (overlaps(read, region, false)) {
+				writer.writeRecord(read);
+			}
+			else if (region.fullyLeftOf(read.ref_id, read.mate_position)) {
+				i++;
+				if (i >= regions.length) break;
+				region = regions[i];
+			}
+		}
+	}
+
+	
+	writefln("Completed. Half mapped reads written to %s.", hm_out);
+	writefln("           Half unmapped reads written to %s.", hu_out);
+	writefln("           Fully unmapped reads written to %s.", au_out);
 }
